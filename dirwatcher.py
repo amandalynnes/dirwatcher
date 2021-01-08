1fe2cba854d08be903f53da9e849144b9e7da991
Amanda Simmons <carpe737diem@gmail.com>
2021-01-08 10:19:11 -0500

    made sample clock LRP, cleaned up code a bit, added comments, argparser and empty docstrings

--- dirwatcher.py
+++ dirwatcher.py
@@ -9,31 +9,56 @@ import sys
 import time
 import os
 
+# Success Criteria
+# Use all best practices that have been taught so far: docstrings, PEP8,
+# clean and readable code, and meaningful commit messages
+# Have a demonstrable OS signal handler
+# Log messages for files containing "magic text"
+# Handle and log different exceptions such as "file not found",
+# "directory does not exist", as well as handle and report
+# top-level unknown exceptions so that your program stays alive
+# Include a startup and shutdown banner in your logs and report
+# the total runtime (uptime) within your shutdown log banner
+# (please see the hints below if you don't understand what a logging banner is)
+# Read the rubric!
+
 
 def search_for_magic(filename, start_line, magic_string):
-    # Your code here
+    """ """
+
     return
 
 
 def watch_directory(path, magic_string, extension, interval):
-    # Your code here
+    """ """
+
     return
 
 
 def create_parser():
-    # Your code here
+    # """Creates an argument parser object."""
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('', help='')
+    # parser.add_argument('', help=')
+
+    # return parser
     return
 
 
 def signal_handler(sig_num, frame):
-    # Your code here
+    """ """
+
     return
 
 
 def main(args):
+    """ """
+    print(f'my PID is: {os.getpid()} ')
     while True:
-        print(f'my PID is: {os.getpid()} ')
-        time.sleep(1)
+        print('tick')
+        time.sleep(2)
+        print('tock')
+        time.sleep(2)
     return
 
 
